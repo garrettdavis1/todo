1aeb596259e6c70e9c8fd367b8a09633a0345402
garrettdavis1 <garrett.davis@washburn.edu>
2017-04-28 13:16:55 -0500

    Add files via upload

---
+++ Day.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FinalProject;
+
+public final class Day {
+
+    private String name;
+
+    public Day() {
+    }
+
+    public Day(String name) {
+        setName(name);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- ToDo.java
+++ ToDo.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ToDo;
+package FinalProject;
 
+import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -12,43 +13,49 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.geometry.HPos;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-
 public class ToDo extends Application {
-ArrayList Week;
+//initialize array lists
+
+    ArrayList DaysofWeek = new ArrayList<Day>();
+    ArrayList TextInFields = new ArrayList<String>();
+    ArrayList tf= new ArrayList<TextField>();
+    
+    
     @Override // Override the start method in the Application class
     public void start(Stage primaryStage) {
+        //initialize stage, buttons, and panes
         Stage stage1 = new Stage(); // Create a new stage
-
+        Stage stage2 = new Stage();
+        stage2.setTitle("To Do");
         stage1.setTitle("Set To Do"); // Set the stage title
-        //grid pane/text enter
+        BorderPane bPane = new BorderPane();
         GridPane pane = new GridPane();
+        VBox showlist = new VBox();
+        HBox addButtons = new HBox();
         pane.setAlignment(Pos.CENTER);
         pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
         pane.setHgap(5.5);
         pane.setVgap(5.5);
-
         Button add = new Button("Add");
-        add.setAlignment(Pos.BOTTOM_RIGHT);
         Button clear = new Button("Clear");
-        clear.setAlignment(Pos.BOTTOM_CENTER);
-        ArrayList DaysofWeek=new ArrayList<String>();
+
+        //initialize the days of week array just has the names of mon-fri
         DaysofWeek.add("Monday");
         DaysofWeek.add("Teusday");
         DaysofWeek.add("Wednesday");
@@ -56,35 +63,50 @@ ArrayList Week;
         DaysofWeek.add("Friday");
         DaysofWeek.add("Saturday");
         DaysofWeek.add("Sunday");
-        
-      for(int k=0;k<6;k++){
-          pane.add(new Label(""+DaysofWeek.get(k)),0,k);
-          pane.add(new TextField(),1,k);
-      }
 
-        add.setOnAction(e -> SendOut());
-        clear.setOnAction(e -> Clear());
+        //declare the textfields
+        for (int k = 0; k < 7; k++) {
+            pane.add(new Label("" + DaysofWeek.get(k)), 0, k);
+            pane.add(new TextField(""), 1, k);
+        }
 
+        add.setOnAction(e -> {
+            SendOut();
+        });
+        clear.setOnAction(e -> {
+            Clear();
+        });
 
-        
-      
-       
-        
-        Scene prompt = new Scene(pane);
+        //add nodes to the pane
+        addButtons.getChildren().addAll(add);//clear?! maybe put on the show
+        bPane.setCenter(pane);
+        bPane.setBottom(addButtons);
+        //show the panes and stages
+        Scene prompt = new Scene(bPane);
         stage1.setScene(prompt);
         stage1.show();
-   
+        
+        Text showtxt=new Text("Show here");
+        
+        showlist.getChildren().addAll(showtxt);
+        Scene show = new Scene(showlist);
+        stage2.setScene(show);
+        stage2.show();
+
     }
+//need to add handler classes or do anon inner classes
 
     public static void main(String[] args) {
         launch(args);
     }
 
     public void SendOut() {
-       
+        //sends to an arraylist that holds the string of each day
+       for(int k=0;k<7;k++){
+          String day=DaysofWeek.get(k).getName();
+       }
     }
 
     private void Clear() {
-        "";
     }
 }
